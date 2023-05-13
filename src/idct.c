d8dba924e40e030e98df9474fd8bb730f81cdf50
khalfalf <https://khalfalf@gitlab.ensimag.fr>
2023-05-13 02:22:16 +0200

    idct update

--- src/idct.c
+++ src/idct.c
@@ -4,36 +4,53 @@
 #include <stdbool.h>
 #include <math.h>
 
-#define pi 3.14159265358979323846
 
-float S(int* x,int* y,float phi[8][8]){
+uint8_t S(int x,int y,int16_t tab_freq[8][8]){
+    /* TAB FREQ IS THE 8*8 BLOCK AFTER IZZ AND IQ */
     float sum=0;
     for (int i=0;i<8;i++){
         for (int j=0;j<8;j++){
             if (i==0 && j==0){
-                sum+=(1/2)*cos((2*(*x)+1)*i*pi/16)*cos((2*(*y)+1)*j*pi/16)*phi[i][j];
+                sum+=(1/2)*cos((2*x+1)*i*M_PI/16)*cos((2*y+1)*j*M_PI/16)*tab_freq[i][j];
             }
             else if ((i==0 && j!=0) || (i!=0 && j==0)){
-                sum+=(1/sqrt(2))*cos((2*(*x)+1)*i*pi/16)*cos((2*(*y)+1)*j*pi/16)*phi[i][j];
+                sum+=(1/sqrt(2))*cos((2*x+1)*i*M_PI/16)*cos((2*y+1)*j*M_PI/16)*tab_freq[i][j];
             }
             else{
-                sum+=cos((2*(*x)+1)*i*pi/16)*cos((2*(*y)+1)*j*pi/16)*phi[i][j];
+                sum+=cos((2*x+1)*i*M_PI/16)*cos((2*y+1)*j*M_PI/16)*tab_freq[i][j];
             }
 
 
         }
     }
-    return sum/sqrt(16);
+    float sum_total=sum/4+128;
+    if (sum_total<0) {printf("s= %f , %u\n",(sum/4)+128,0);
+        return 0;}
+    else if (sum_total>255) { printf("s= %f , %u\n",(sum/4)+128,255);
+        return (uint8_t) 255;}
+    else {printf("s= %f , %u\n",(sum/4)+128,(uint8_t)sum/4+128);
+    return (uint8_t) sum_total;}
 
 }
 
-void idct(float S[8][8],uint8_t converted[8][8]){
+void idct(uint8_t out[8][8],int16_t tab_freq[8][8]){
     for (int i=0;i<8;i++){
         for (int j=0;j<8;j++){
-            uint8_t value=(uint8_t)(S[i][j]+128.0f);
-            if (value<0) value=0;
-            else if (value>255) value=255;
-            converted[i][j]=(uint8_t) value;
+            uint8_t value=S(i,j,tab_freq);
+            out[i][j]=value;
         }
     }
+}
+
+int main(void){
+    int16_t freq[8][8]={{6,-9,-1,-1,0,0,0,0},{4,1,-1,1,1,0,0,1},{-7,0,0,-1,0,0,0,0},{2,1,-1,0,0,0,0,-1},{-1,0,0,0,0,0,0,0},{1,0,-1,0,0,0,0,0},{0,0,0,0,0,0,0,0},{1,0,0,0,0,0,0,0}};
+    uint8_t out[8][8];
+    printf("entier: %i\n",freq[0][1]);
+    idct(out,freq);
+    for (int i=0;i<8;i++){
+        for (int j=0;j<8;j++){
+            printf("out[%i,%i]= %02x\n",i,j,out[i][j]);
+        }
+    }
+    
 }
