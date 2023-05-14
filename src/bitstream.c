28cef8df8636a0b74c732f029f0bec3351556630
khalfalf <https://khalfalf@gitlab.ensimag.fr>
2023-05-15 00:52:40 +0200

    bitstream update

--- src/bitstream.c
+++ src/bitstream.c
@@ -1,22 +1,79 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <stdint.h>
+#include <stdbool.h>
+
+
 
 /* EXTRACT BITSTREAM INTO A STRING IN ORDER TO READ BIT BY BIT */
 
-void create_bitstream(FILE* file){
+void create_stream(FILE* file){
     
-    /*CREATES BITSTREAM TEXT*/
+    /*CREATES BITSTREAM TEXT AND ELIMINATE BYTESTUFFING*/
     
     FILE* bitstream=fopen("bitstream.txt","wb");
-    uint16_t buffer_16;
-    while (fread(&buffer_16,sizeof(uint16_t),1,file)==1){
-        
-        buffer_16=htons(buffer_16);
-        if (buffer_16==0xffd9) break;
-        for (int i = 0; i < 16; i++) { // loop through each bit of the 16-bit data
-            int bit = (buffer_16 >> (15 - i)) & 1; // extract the i-th bit from the data
-            fprintf(bitstream, "%d", bit); // write the binary representation of the bit to the file
+    uint8_t byte, next_byte;
+    bool last_byte_ff = false;
+    while (fread(&byte, sizeof(uint8_t), 1, file) == 1){
+        if (byte == 0xff){
+            if (fread(&next_byte, sizeof(uint8_t), 1, file) == 1){
+                if (next_byte == 0x00){
+                    /* Byte stuffing found, skip 0x00 and write only 0xff */
+                    last_byte_ff = true;
+                    continue;
+                } else if (next_byte == 0xd9){
+                    /* End of image marker found, stop processing*/
+                    break;
+                } else {
+                    /* No byte stuffing, write both bytes to output*/
+                    if (last_byte_ff) {
+                        fprintf(bitstream, "BYTE: %02X\n", 0xff);
+                        /*Write the bits in the .txt file*/
+                        for (int i = 0; i < 8; i++) {
+                            int bit = (0xff >> (7 - i)) & 1;
+                            fprintf(bitstream, "%d", bit);
+                        }
+                        fprintf(bitstream, "\n");
+                        last_byte_ff = false;
+                    }
+                    fprintf(bitstream, "BYTE: %02X\n", byte);
+                    /*Write the bits in the .txt file*/
+                    for (int i = 0; i < 8; i++) {
+                        int bit = (byte >> (7 - i)) & 1;
+                        fprintf(bitstream, "%d", bit);
+                    }
+                    fprintf(bitstream, "\n");
+                }
+            } else {
+                /* End of file reached, write last byte to output */
+                fprintf(bitstream, "BYTE: %02X\n", byte);
+                /*Write the bits in the .txt file*/
+                for (int i = 0; i < 8; i++) {
+                    int bit = (byte >> (7 - i)) & 1;
+                    fprintf(bitstream, "%d", bit);
+                }
+                fprintf(bitstream, "\n");
+                break;
+            }
+        } else {
+            /* No byte stuffing, write byte to output*/
+            if (last_byte_ff) {
+                fprintf(bitstream, "BYTE: %02X\n", 0xff);
+                /*Write the bits in the .txt file*/
+                for (int i = 0; i < 8; i++) {
+                    int bit = (0xff >> (7 - i)) & 1;
+                    fprintf(bitstream, "%d", bit);
+                }
+                fprintf(bitstream, "\n");
+                last_byte_ff = false;
+            }
+            fprintf(bitstream, "BYTE: %02X\n", byte);
+            /*Write the bits in the .txt file*/
+            for (int i = 0; i < 8; i++) {
+                int bit = (byte >> (7 - i)) & 1;
+                fprintf(bitstream, "%d", bit);
+            }
+            fprintf(bitstream, "\n");
         }
     }
     fclose(bitstream);
