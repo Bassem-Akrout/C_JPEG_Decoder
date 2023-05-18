a941708099b7ae3090757933ea8d6536a84252b4
khalfalf <https://khalfalf@gitlab.ensimag.fr>
2023-05-18 02:07:48 +0200

    forgot this ..

--- include/decoded_image_creator.h
+++ include/decoded_image_creator.h
@@ -1,7 +1,12 @@
 #ifndef _DECODE_IMAGE_CREATOR_H_
 #define _DECODE_IMAGE_CREATOR_H_
+#include "jpeg_reader.h"
 
+/*FUNCTIONS*/
+    // FOR PGM 
+extern void blackandwhite(struct SOF* sof,char* jpeg_name);
 
-/*FONCTIONS*/
+    // FOR PPM
+extern void rainbow(struct SOF* sof,char* jpeg_name);
 
 #endif /*_DECODE_IMAGE_CREATOR_H_*/
