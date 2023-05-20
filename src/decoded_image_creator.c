7b54435fb2218e574076513743ed24f1aee4a7e9
Mouez Jaafoura <ensimag@ensi-libre-service-065.ensimag.fr>
2023-05-20 14:11:53 +0200

    B/W images working

--- src/decoded_image_creator.c
+++ src/decoded_image_creator.c
@@ -27,22 +27,33 @@ char * create_name_file(char * jpeg_name, uint8_t nb_components){
 void blackandwhite(uint8_t** MCUs_RGB,struct SOF* sof,char* jpeg_name){
     
     // INTITIALIZE PGM IMAGE
-
     char *new_file = create_name_file(jpeg_name,1);
     FILE *new_image = fopen(new_file,"wb");
     char* format="P5";
     char* rgb="255";
+    uint32_t MCU_hori_number =(1+(uint32_t)ceil(sof->width/(sof->sampling_horizontal[0]*8)));
+    uint32_t MCU_vertical_number=(1+(uint32_t)ceil(sof->height/(sof->sampling_vertical[0]*8)));
     fprintf(new_image,"%s\n",format);
-    fprintf(new_image,"%u %u\n",sof->width,sof->height);
+    fprintf(new_image,"%u %u\n",(sof->width-(sof->width%8)),(sof->height-(sof->height%8)));
     fprintf(new_image,"%s\n",rgb);
     
-    // WRITE CONTENT IN PGM IMAGE
 
-    for (uint32_t l=0;l<(uint32_t)floor(sof->height/(sof->sampling_vertical[0]*8));l++)
+    // WRITE CONTENT IN PGM IMAGE
+    if((ceil(sof->width/(sof->sampling_horizontal[0]*8)))!=((double)(sof->width)/(double)((sof->sampling_horizontal[0]*8)))){
+        for (uint32_t l=0;l<MCU_vertical_number-1;l++)
             for (int j = 0; j < 8; j++)
-                for (uint32_t i = 0; i < (uint32_t)floor(sof->width/(sof->sampling_horizontal[0]*8));i++)
+                for (uint32_t i = 0; i < MCU_hori_number-1 ;i++)
                     for (int k = 0; k < 8;k++)
-                        fwrite(&(MCUs_RGB[i+l*(uint32_t)floor(sof->width/(sof->sampling_horizontal[0]*8))][j*8+k]),sizeof(uint8_t),1,new_image);
+                        fwrite(&(MCUs_RGB[i+l*(MCU_hori_number)][j*8+k]),sizeof(uint8_t),1,new_image);}
+    else{
+        for (uint32_t l=0;l<MCU_vertical_number-1;l++)
+            for (int j = 0; j < 8; j++)
+                for (uint32_t i = 0; i < MCU_hori_number -1;i++)
+                    for (int k = 0; k < 8;k++)
+                        fwrite(&(MCUs_RGB[i+l*(MCU_hori_number-1)][j*8+k]),sizeof(uint8_t),1,new_image);
+    }
+    
+                        
     fclose(new_image);
     free(new_file);
 
